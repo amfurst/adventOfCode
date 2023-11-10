61d56b225cdead52041818042f9b1b8c6293fe2e
Anthony Furst <amfurst@gmail.com>
2023-11-10 17:06:31 -0500

    2022 Day 1 Complete

--- src/main/kotlin/com/amfurst/adventOfCode/year2022/day1/App.kt
+++ src/main/kotlin/com/amfurst/adventOfCode/year2022/day1/App.kt
@@ -1,4 +1,24 @@
 package com.amfurst.adventOfCode.year2022.day1
 
-class App {
+fun main() {
+    val inputLines = object {}.javaClass.getResourceAsStream("/2022/day1/input.txt")?.bufferedReader()?.readLines()
+
+    val elves = inputLines?.fold(mutableListOf(Elf())) { elves, item ->
+        if (item.isEmpty()) {
+            elves.add(Elf())
+        } else {
+            elves.last() add item.toInt()
+        }
+        elves
+    }
+
+    println(elves?.maxOfOrNull { it.getTotal() })
+}
+
+class Elf {
+    private val items: MutableList<Int> = mutableListOf()
+
+    infix fun add(item: Int) = items.add(item)
+
+    fun getTotal() = items.reduce {acc, next -> acc + next}
 }
